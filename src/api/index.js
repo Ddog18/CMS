82c1a07dd1854c48f2c9595a1b7231de8e2aa05b
Ddog18 <53813021+Ddog18@users.noreply.github.com>
2020-06-05 11:11:58 +0800

    Add files via upload

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import Admin from './modules/admin.js';
+import User from './modules/user.js'
+import Account from './modules/account.js'
+import Goods from './modules/goods.js'
+import Order from './modules/order.js'
+import Power from './modules/power.js'
+import Auth from './modules/auth.js'
+import Upload from'./modules/upload.js'
+import City from './modules/city.js'
+import Category from './modules/category.js'
+
+ 
+ export{
+	 Admin,
+	 User,
+	 Account,
+	 Goods,
+	 Order,
+	 Power,
+	 Auth,
+	 Upload,
+	 City,
+	 Category
+ }
